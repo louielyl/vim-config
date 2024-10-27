7d298e8e6442fbb6ecacb7f81600740f1a45b512
Louie Lee <louie.lee@datax.io>
2024-10-27 15:30:28 +0800

    feat: show filtered items in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -61,11 +61,9 @@ return {
     filesystem = {
       bind_to_cwd = false,
       follow_current_file = true,
-      -- filtered_items = {
-      --   visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-      --   hide_dotfiles = false,
-      --   hide_gitignored = false,
-      -- },
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      },
     },
     window = {
       mappings = {
